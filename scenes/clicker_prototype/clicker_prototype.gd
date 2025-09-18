198b6e536269fbbe489cf3e869b430523f615bee
AlanikREDAWN <lilia.dostie@socprep.org>
2025-09-18 16:20:20 -0400

    saving data

--- scenes/clicker_prototype/clicker_prototype.gd
+++ scenes/clicker_prototype/clicker_prototype.gd
@@ -4,16 +4,14 @@ extends Control
 @export
 var label : Label
 
-var clicks : int = 0
-
 func _ready() -> void:
 	update_label()
 
 func update_label() -> void:
-	label.text = "Meows: %s" %clicks
+	label.text = "Meows: %s" %Game.ref.data.meows
 
 func increase_clicks() -> void:
-	clicks += 1
+	Game.ref.data.meows += 1
 	update_label()
 
 func _on_texture_button_pressed() -> void:
---
+++ scripts/data.gd
@@ -0,0 +1,4 @@
+class_name Data
+
+
+var meows : int = 0
