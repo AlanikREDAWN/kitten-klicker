cedfaa4becb984be3ff2c53bf33e9ec3bc87b942
AlanikREDAWN <lilia.dostie@socprep.org>
2025-09-20 16:38:34 -0400

    art cat

---
+++ scenes/art_cat.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+signal increase_meow
+
+var time_since_last_increase = 0
+#var art_cat_unlocked = false
+
+func _ready() -> void:
+	time_since_last_increase = 0
+	
+func _process(delta: float) -> void:
+	#if art_cat_unlocked:
+		time_since_last_increase += delta
+		
+		if time_since_last_increase >= 1:
+			time_since_last_increase -= 1
+			increase_meow.emit()
