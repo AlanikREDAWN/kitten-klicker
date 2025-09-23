4a0f5a16d3a6d3648843ba2aaecd4c358588d446
AlanikREDAWN <lilia.dostie@socprep.org>
2025-09-23 14:31:25 -0400

    made viewport bigger and had to change everything's size

---
+++ scripts/Global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var meows = 10000
+var items = {
+	0: {
+		"Name": "Art Cat",
+		"Desc": "+1 Meow per second",
+		"Cost": 10,
+	},
+	1: {
+		"Name": "Beige Cat",
+		"Desc": "+2 Meows per second",
+		"Cost": 20,
+	},
+}
